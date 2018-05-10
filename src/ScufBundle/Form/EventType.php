01c6dc098f1a2cc432a29e5e120df6414ba54239
Alexandre Simonin <a.simonin69@gmail.com>
2018-05-10 19:57:34 +0200

    ADD Event form

--- src/ScufBundle/Form/EventType.php
+++ src/ScufBundle/Form/EventType.php
@@ -2,53 +2,38 @@
 
 namespace ScufBundle\Form;
 
-use ScufBundle\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ActionType extends AbstractType
+class EventType extends AbstractType
 {
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('type', TextType::class)
-            ->add('created', DateTimeType::class, [
-                'model_timezone' => 'UTC',
-                'view_timezone'  => 'Europe/Paris'
-            ])
-            ->add('updated', DateTimeType::class, [
-                'model_timezone' => 'UTC',
-                'view_timezone'  => 'Europe/Paris'
-            ])
+            ->add('title', TextType::class)
+            ->add('all_day', IntegerType::class)
             ->add('start', DateTimeType::class, [
                 'widget' => 'single_text',
-                'format' => 'dd-MM-yyyy HH:mm',
-                //'format' => 'yyyy-MM-dd HH:mm:ss',
+                'format'  => 'dd-MM-yyyy HH:mm'
             ])
             ->add('end', DateTimeType::class, [
                 'widget' => 'single_text',
-                'format' => 'dd-MM-yyyy HH:mm',
-                //'format' => 'yyyy-MM-dd HH:mm:ss',
+                'format' => 'dd-MM-yyyy HH:mm'
             ])
-            ->add('status', IntegerType::class)
-            ->add('view', IntegerType::class)
-            ->add('user', EntityType::class, array('class' => User::class))
-            ->add('recipient', EntityType::class, array('class' => User::class))
-            ->add('justification', TextType::class)
-    ;
+            ->add('location', TextType::class)
+            ->add('bg_color', TextType::class)
+            ->add('border_color', TextType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'ScufBundle\Entity\Action',
+            'data_class' => 'ScufBundle\Entity\Event',
             'csrf_protection' => false,
         ));
     }
